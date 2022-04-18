cbaf90b64a4d39e18c27f96a738d44f7c34e93bb
LyeWeiKang <92768605+LyeWeiKang@users.noreply.github.com>
2022-04-18 22:40:07 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,11 +1,8 @@
+Question1.java
 public class Question1
 {
   public static void main(String[] args)
   {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+     Hint: System.out.println("Hello World")
   }
 }
