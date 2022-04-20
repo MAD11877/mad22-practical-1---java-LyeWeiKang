8257467c3862b93f684f178a8610a7f50df999f0
LyeWeiKang <92768605+LyeWeiKang@users.noreply.github.com>
2022-04-20 12:24:17 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,25 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter the number of integers:");
+    int number=in.nextInt();
+    int[] arr=new int[number]
+    for(int i=0;i<number;i++){
+      System.out.println("Enter a number:");
+      arr[i]=in.nextInt();   
+    }
+    Arrays.sort(arr);
+    for(i=0;i<arr.length;i++){
+      int count=1;
+      for(int j=i+1;j<arr.length;j++){
+        if (arr[i]==arr[j]){
+          count+=1;
+        }
+        else{
+          break;
+        }
+      }
+    }
     
   }
 }
